8f5b385f08f3e0043096dd8b20bdfaa8489b1f99
ash.blade <ashen.blade.dev@gmail.com>
2022-07-25 22:14:44 +0300

    add endpoint to search products by name

--- src/WannaEat.Web/Controllers/ProductsController.cs
+++ src/WannaEat.Web/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WannaEat.Web.Infrastructure.Attributes;
 using WannaEat.Web.Models;
 using WannaEat.Web.Services;
 
@@ -19,10 +20,10 @@ public class ProductsController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet]
+    [HttpGet("")]
     public async Task<ActionResult<IEnumerable<Product>>> GetFoodPaged([Required(ErrorMessage = "Specify page size")]
                                                                     [FromQuery(Name = "s")]
-                                                                    [Range(1, int.MaxValue)]
+                                                                    [Range(1, 100)]
                                                                     int pageSize, 
                                                                     [Required(ErrorMessage = "Specify page number")]
                                                                     [FromQuery(Name = "n")]
@@ -37,8 +38,24 @@ public class ProductsController : ControllerBase
         return Ok(products);
     }
 
+    [HttpGet("search")]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProductsByName([FromQuery(Name = "name")][Required][MinLength(3)]
+                                                                            string name, 
+                                                                            [FromQuery(Name = "max")][Range(1, 100)]
+                                                                            int max = 10)
+    {
+        _logger.LogInformation("Search product by name '{ProductName}' requested with max amount {MaxAmount}", name, max);
+        var products = await _context.Products
+                                     .Where(p => EF.Functions.ILike(p.Name, name))
+                                     .Take(max)
+                                     .ToListAsync();
+        _logger.LogDebug("{ProductsAmount} found by query {ProductName}", products.Count, name);
+        return Ok(products);
+    }
+
     [HttpGet("{id:int}")]
-    public async Task<ActionResult<Product>> GetProductByIdAsync(int id)
+    public async Task<ActionResult<Product>> GetProductByIdAsync([FromRoute(Name = "id")][Required]
+                                                                 int id)
     {
         _logger.LogTrace("Product with id {ID} requested", id);
         var product = await _context.Products.SingleOrDefaultAsync(p => p.Id == id);
