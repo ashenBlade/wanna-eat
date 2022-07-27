c1dff9947d0fddfdeee5cb90ee3db772a33a579c
ash.blade <ashen.blade.dev@gmail.com>
2022-07-27 21:19:35 +0300

    add invisible scrollbar

--- src/WannaEat.Web/ClientApp/src/components/FoodList/FoodList.tsx
+++ src/WannaEat.Web/ClientApp/src/components/FoodList/FoodList.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {Food} from "../../entities/food";
+import './FoodList.tsx.css'
 
 export interface FoodListProps {
     foods: Food[],
@@ -9,14 +10,20 @@ export interface FoodListProps {
 const FoodList: FC<FoodListProps> = ({foods}) => {
     
     return (
-        <ul className={'list-group'}>
-            {foods.map(f => (
-                <li className={'list-group-item'}>
-                    {f.name}
-                </li>
-                )
-            )}
-        </ul>
+        <div className={'h-100'}>
+            <div className={'p-1'}>
+                <div className={'bg-light p-2 rounded-1 scroll-list'}>
+                    <ul className={'list-group rounded-1'}>
+                        {foods.map(f => (
+                                <li className={'list-group-item'}>
+                                    {f.name}
+                                </li>
+                            )
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 };
 
