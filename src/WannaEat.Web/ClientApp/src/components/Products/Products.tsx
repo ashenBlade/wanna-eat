e5c32b9568ef73ccad6d55bd7523b543c01078f7
ash.blade <ashen.blade.dev@gmail.com>
2022-07-27 20:39:26 +0300

    move Products page to own folder

---
+++ src/WannaEat.Web/ClientApp/src/components/Products/Products.tsx
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import {Product} from "../../entities/product";
+import {Dish} from "../../entities/dish";
+import {IProductRepository} from "../../services/products.repository";
+import {IDishRepository} from "../../services/dish.repository";
+import {IFoodService} from "../../services/food.service";
+import './Products.tsx.css';
+import FoodList from "../FoodList/FoodList";
+
+interface ProductsPageProps {
+    productsRepository: IProductRepository
+    dishesRepository: IDishRepository
+    foodService: IFoodService
+}
+
+const Products: React.FC<ProductsPageProps> = ({productsRepository, dishesRepository, foodService}) => {
+    const [products, setProducts] = useState<Product[]>([])
+    const [dishes, setDishes] = useState<Dish[]>([])
+    
+    useEffect(() => {
+        productsRepository.getProductsAsync(1, 10).then(p => setProducts(p))
+        dishesRepository.getDishesAsync(1, 10).then(d => setDishes(d))
+    }, [])
+    
+    const searchOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const name = e.target.value
+        if (!name) {
+            productsRepository.getProductsAsync(1, 10).then(p => setProducts(p))
+            return
+        }
+        
+        productsRepository.findWithName(name).then(p => setProducts(p))
+    } 
+    
+    
+    return (
+        <div className={'h-100'}>
+            <div className={'double-column h-100'}>
+                <div className={'pb-1 d-flex justify-content-between align-items-center'}>
+                    <input className={'form-control'} type={'search'}
+                           placeholder={'Что искать?'}
+                           onChange={searchOnChange}/>
+                        <i className={'fa fa-solid fa-gear fa-lg rotate-90-hover'}
+                           // onMouseOver={e => e.currentTarget.classList.add('asdf')}
+                           // onMouseLeave={e => e.currentTarget.classList.remove('asdf')}
+                        ></i>
+                </div>
+                <div/>
+                <div className={'grounded'}>
+                    <FoodList foods={products}/>
+                </div>
+                <div className={'grounded'}>
+                    <FoodList foods={dishes}/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
