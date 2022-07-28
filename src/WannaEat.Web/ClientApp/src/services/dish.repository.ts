6e0188740a74a34bc2a29d762b7aac0b9da41b00
ash.blade <ashen.blade.dev@gmail.com>
2022-07-28 20:07:11 +0300

    add dishes repository implementation

--- src/WannaEat.Web/ClientApp/src/services/dish.repository.ts
+++ src/WannaEat.Web/ClientApp/src/services/dish.repository.ts
@@ -14,11 +14,19 @@ export class DishRepository implements IDishRepository{
         {id: 5, name: 'Cottage cheesecake'}
     ]
     
-    getDishByIdAsync(id: number): Promise<Dish | null> {
-        return Promise.resolve(DishRepository.defaultDishes.find(d => d.id === id) ?? null);
+    async getDishByIdAsync(id: number): Promise<Dish | null> {
+        return await fetch(`/api/v1/dishes/${id}`).then(res => res.json());
     }
 
-    getDishesAsync(pageNumber: number, pageSize: number): Promise<Dish[]> {
-        return Promise.resolve([...DishRepository.defaultDishes]);
+    async getDishesAsync(pageNumber: number, pageSize: number): Promise<Dish[]> {
+        if (pageNumber < 1) {
+            throw new Error('Page number could not be less than 1. Given: ' + pageNumber);
+        }
+        
+        if (pageSize < 1) {
+            throw new Error('Page size could not be less than 1. Given: ' + pageSize);
+        }
+        
+        return await fetch(`/api/v1/dishes?page-number=${pageNumber}&page-size=${pageSize}`).then(res => res.json());
     }
 }
