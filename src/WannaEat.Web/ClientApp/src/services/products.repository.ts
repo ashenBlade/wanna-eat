939cf2c02311b27d948a264f8399b32188e2f5e6
ash.blade <ashen.blade.dev@gmail.com>
2022-07-28 20:13:38 +0300

    add products repository implementation with api

--- src/WannaEat.Web/ClientApp/src/services/products.repository.ts
+++ src/WannaEat.Web/ClientApp/src/services/products.repository.ts
@@ -17,17 +17,28 @@ export class ProductRepository implements IProductRepository {
     ]
     
     getProductById(id: number): Promise<Product | null> {
-        return Promise.resolve(ProductRepository.defaultProducts.find(p => p.id === id) ?? null);
+        return fetch(`api/v1/products/${id}`).then(res => res.json());
     }
 
     getProductsAsync(pageNumber: number, pageSize: number): Promise<Product[]> {
-        const start = (pageNumber - 1) * pageSize;
-        const end = start + pageSize
-        return Promise.resolve([...ProductRepository.defaultProducts.slice(start, end)]);
+        if (pageNumber < 1) {
+            throw new Error('Page number could not be less than 1. Given: ' + pageNumber);
+        }
+        
+        if (pageSize < 1) {
+            throw new Error('Page size could not be less than 1. Given: ' + pageSize);
+        }
+        return fetch(`/api/v1/products?page-size=${pageSize}&page-number=${pageNumber}`).then(res => res.json());
     }
 
-    findWithName(name: string): Promise<Product[]> {
-        name = name.toLowerCase()
-        return Promise.resolve(ProductRepository.defaultProducts.filter(p => p.name.toLowerCase().indexOf(name) !== -1));
+    findWithName(name: string, max: number = 10): Promise<Product[]> {
+        if (name.length < 3) {
+            throw new Error('Product length could not be less than 3. Given: ' + name);
+        }
+        
+        if (max < 1 || 100 < max) {
+            throw new Error('Max return range must be in range 1 to 100. Given: ' + max);
+        }
+        return fetch(`/api/v1/products/search?name=${encodeURIComponent(name)}&max=${max}`).then(res => res.json())
     }
 }
