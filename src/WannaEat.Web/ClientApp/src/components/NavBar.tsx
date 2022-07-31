4b37c43aecbe473a37b2c47dc35d8ef3702dcbca
ash.blade <ashen.blade.dev@gmail.com>
2022-07-31 10:13:37 +0300

    change NavBar to blue

--- src/WannaEat.Web/ClientApp/src/components/NavBar.tsx
+++ src/WannaEat.Web/ClientApp/src/components/NavBar.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 const NavBar = () => {
     return (
         <>
-            <div style={{
-                backgroundColor: 'red'
-            }} className={'h-auto'}>
+            <div className={'h-auto bg-primary'}>
                 
             <div className={'container-lg container-fluid shadow bg-red'}>
                 <nav className={'navbar navbar-expand-lg navbar-dark text-white-50'}>
