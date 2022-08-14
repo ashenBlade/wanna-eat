d0f04928faa8f31d37dd857a60efbd59f6259eb1
ash.blade <ashen.blade.dev@gmail.com>
2022-08-14 11:20:50 +0300

    add initial migration

---
+++ src/WannaEat.Web/Migrations/20220814065937_InitialMigration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace WannaEat.Web.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    NameSearchVector = table.Column<NpgsqlTsVector>(type: "tsvector", nullable: false)
+                        .Annotation("Npgsql:TsVectorConfig", "russian")
+                        .Annotation("Npgsql:TsVectorProperties", new[] { "Name" })
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_NameSearchVector",
+                table: "Ingredients",
+                column: "NameSearchVector")
+                .Annotation("Npgsql:IndexMethod", "GIN");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+        }
+    }
+}
