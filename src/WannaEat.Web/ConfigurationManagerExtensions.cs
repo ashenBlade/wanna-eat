43b1d9553cb89c05507e6f37bdfa5da058176412
ash.blade <ashen.blade.dev@gmail.com>
2022-08-14 20:37:56 +0300

    update Dockerfile

--- src/WannaEat.Web/ConfigurationManagerExtensions.cs
+++ src/WannaEat.Web/ConfigurationManagerExtensions.cs
@@ -4,6 +4,6 @@ public static class ConfigurationManagerExtensions
 {
     public static bool IsHeroku(this ConfigurationManager manager)
     {
-        return manager.GetValue<bool>("HEROKU_APP");
+        return manager.GetValue<int>("HEROKU_APP") == 1;
     }
 }
