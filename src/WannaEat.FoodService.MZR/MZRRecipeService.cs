e5f270a2f979aced0aff7f305e9533a67d3f5758
ash.blade <ashen.blade.dev@gmail.com>
2022-08-19 21:38:26 +0300

    refactor implementation for MZRRecipeService

--- src/WannaEat.FoodService.MZR/MZRRecipeService.cs
+++ src/WannaEat.FoodService.MZR/MZRRecipeService.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using WannaEat.Domain.Entities;
 using WannaEat.Domain.Interfaces;
 using WannaEat.FoodService.MZR.Models;
+using Recipe = WannaEat.Domain.Entities.Recipe;
 
 namespace WannaEat.FoodService.MZR;
 
@@ -19,16 +20,20 @@ public class MZRRecipeService: IRecipeService
         _logger = logger;
     }
 
-    public async Task<IEnumerable<Domain.Entities.Recipe>> GetRecipesForIngredients(IEnumerable<Ingredient> ingredients, CancellationToken token)
+    private static IEnumerable<string> NormalizeIngredientNames(IEnumerable<Ingredient> ingredients)
+    {
+        return ingredients
+           .SelectMany(i => i.Name
+                             .ToLower()
+                             .Split(' ', StringSplitOptions.RemoveEmptyEntries));
+    }
+
+    public async Task<IEnumerable<Recipe>> GetRecipesForIngredients(IEnumerable<Ingredient> ingredients, CancellationToken token)
     {
         try
         {
-            using var message =
-                new HttpRequestMessage(HttpMethod.Post, "https://fs2.tvoydnevnik.com/api2/recipe_search/search");
-            message.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            message.Content = GetPayload(ingredients);
-            using var response =
-                await _client.SendAsync(message, token);
+            using var message = CreateRequestAllRecipesMessage(ingredients);
+            using var response = await _client.SendAsync(message, token);
             response.EnsureSuccessStatusCode();
             var mzr = await response
                            .Content
@@ -36,28 +41,39 @@ public class MZRRecipeService: IRecipeService
             if (mzr is null)
             {
                 _logger.LogWarning("Could not parse JSON from response");
-                return Enumerable.Empty<Domain.Entities.Recipe>();
+                return Enumerable.Empty<Recipe>();
             }
-            var recipes = mzr.Result
-                             .Recipes
-                             .Select(f => new Domain.Entities.Recipe(f.Food.Name, new Uri($"{f.Domain}{f.Food.Link}"), new Uri(f.Food.ImageUrl)));
-            return recipes;
+            var recipes = mzr.Result?
+                             .Recipes?
+                             .Select(f => f.ToDomainRecipe());
+            return recipes ?? Enumerable.Empty<Recipe>();
         }
         catch (TaskCanceledException canceled)
         {
-            _logger.LogWarning(canceled, "Could not download and parse recipes. Task was cancelled from cancellation token");
-            return Enumerable.Empty<Domain.Entities.Recipe>();
+            _logger.LogWarning(canceled, "Could not download and parse recipes. "
+                                       + "Task was cancelled from cancellation token");
+            return Enumerable.Empty<Recipe>();
         }
         catch (HttpRequestException http)
         {
-            _logger.LogWarning(http, "Could not download recipes. Error while HTTP request");
-            return Enumerable.Empty<Domain.Entities.Recipe>();
+            _logger.LogWarning(http, "Could not download recipes. "
+                                   + "Error while HTTP request");
+            return Enumerable.Empty<Recipe>();
         }
+    }
+
+    private static HttpRequestMessage CreateRequestAllRecipesMessage(IEnumerable<Ingredient> ingredients)
+    {
+        var message =
+            new HttpRequestMessage(HttpMethod.Post, "https://fs2.tvoydnevnik.com/api2/recipe_search/search");
+        message.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        message.Content = GetPayload(ingredients);
+        return message;
     }
 
     private static FormUrlEncodedContent GetPayload(IEnumerable<Ingredient> ingredients)
     {
-        var ingredientsString = string.Join(',', ingredients.Select(i => i.Name.Trim(' ')));
+        var ingredientsString = string.Join(',', NormalizeIngredientNames(ingredients));
         return new FormUrlEncodedContent(new KeyValuePair<string, string>[]
                                          {
                                              new("jwt", "false"), 
--- src/WannaEat.FoodService.MZR/Models/Recipe.cs
+++ src/WannaEat.FoodService.MZR/Models/Recipe.cs
@@ -8,4 +8,12 @@ internal class Recipe
     public Food Food { get; set; }
     [JsonPropertyName("domain")]
     public string Domain { get; set; }
+
+    private string Name => Food.Name;
+    private Uri ImageUrl => new($"{Domain}{Food.ImageUrl}");
+
+    private Uri Source => new($"{Domain}{Food.Link}");
+    
+    public Domain.Entities.Recipe ToDomainRecipe() 
+        => new(Name, Source, ImageUrl);
 }
